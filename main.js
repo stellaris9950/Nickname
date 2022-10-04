7a0ec2cf1e47972852be1abb1a5545c7dff061c2
stellaris9950 <andyzihengwei@gmail.com>
2022-10-04 12:14:52 -0600

    stuff

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 // Triangle Perimeter Assignment Start Code
+function isOdd (number){
+    let result = number / 2;
+    let int = Math.floor(result);
+    let dec = result - int;
+    if (dec === 0){
+      alert("even");
+      stuff = "false"
+    }
+    else {
+      alert("odd")
+      stuff = "true"
+    }
+    return(stuff)
+  }
